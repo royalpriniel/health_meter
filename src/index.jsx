ae2c670824660b9f14fe2aca235b3d8d8852277a
Daniel Somtoochi Dav-Emmanuel <74049793+royalpriniel@users.noreply.github.com>
2026-05-17 17:46:44 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client';
-import {GuestList} from './healthMeter';
+import {HealthMeter} from './healthMeter';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<HealthMeter />);
